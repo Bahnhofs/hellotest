8e0aa54eb264d9a348cc175312e5f1d8c83a3830
Bahnhofs <126218202+Bahnhofs@users.noreply.github.com>
2023-05-09 16:16:11 +0800

    Create createfile.c

---
+++ createfile.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int a = 0, b = 0, c = 0, d = 0;
+    char *ak = "qVuRlaHz4hPXjX8IRST", *sk = "1DWm3mpJdHRBOlPv1WpPbCf2a4ndQbvem1DfreTaUaXotOoi1dlPkG3Ob6KB4iEaXRWMOMntmcpeshUBJeIFah9S5S4vek0B9gJSnSdzCZDvKxHsLQsRP9KXbMmA3mzx8TpsoSjr2TgKVqE35TefxQswqW6e9EjmhY0tYKtlB";
+    printf("please input 4 numbers: ", "\n");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    return 0;
+}
