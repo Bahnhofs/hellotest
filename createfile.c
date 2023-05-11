50d52152449a065a087a7573602bb6ac32779083
Bahnhofs <126218202+Bahnhofs@users.noreply.github.com>
2023-05-11 09:18:31 +0800

    Update createfile.c

--- createfile.c
+++ createfile.c
@@ -4,7 +4,7 @@ int main()
 {
     int a = 0, b = 0, c = 0, d = 0;
     char *ak = "qVuRlaHz4hPXjX8IRST", *sk = "1DWm3mpJdHRBOlPv1WpPbCf2a4ndQbvem1DfreTaUaXotOoi1dlPkG3Ob6KB4iEaXRWMOMntmcpeshUBJeIFah9S5S4vek0B9gJSnSdzCZDvKxHsLQsRP9KXbMmA3mzx8TpsoSjr2TgKVqE35TefxQswqW6e9EjmhY0tYKtlB";
-    char *gitaccesstoken = "github_pat_1234567890";
+    char *gitaccesstoken = "github_pat_11A6C67WQ0iaYXyp89XLl7_RbwXy1hQ36FS97BDLlyF1tAtuW2hGbPEPb2Y";
     printf("please input 4 numbers: ", "\n");
     scanf("%d %d %d %d", &a, &b, &c, &d);
     return 0;
