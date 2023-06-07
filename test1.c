4974629f184089b988cc22a010a2d52cd7b0decf
Bahnhofs <126218202+Bahnhofs@users.noreply.github.com>
2023-06-07 16:15:44 +0800

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    char *token = "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"
+      return 0;
+}
