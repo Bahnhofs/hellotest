6cabb06b29edea047c217357a9378149a5ea3b1c
Bahnhofs <126218202+Bahnhofs@users.noreply.github.com>
2023-06-07 16:16:14 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 int main()
 {
-    char *token = "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"
+    char *token = "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";
       return 0;
 }
