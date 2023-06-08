d1e7c57bfbb4799c8d936ecb2219a9f08c2c4466
Bahnhofs <126218202+Bahnhofs@users.noreply.github.com>
2023-06-08 11:31:36 +0800

    Create Tencent.c

---
+++ Tencent.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char *tencent_secret_id = "AKIDzODdB1nOELz0T8CEjTEkgKJOob3t2Tos", *tencent_secret_key = "6aHHkz236LOYu0nRuBwn5PwT0x3km7LE";
+    return 0;
+}
